e558ca0bfa8a5e3f367fd0d79f6311c750f22014
Or <or@gizra.com>
2015-07-20 15:39:03 +0300

    Adding default labels script

---
+++ bash-scripts/default_labels.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo -n "GitHub User: "
+read USER
+
+echo -n "GitHub Password: "
+read -s PASS
+
+REPO_USER="Gizra"
+
+echo ""
+echo -n "GitHub Repo Name: "
+read REPO_NAME
+
+# Default Labels.
+declare -A LABELS
+LABELS=(
+  ["Merged"]="bfe5bf"
+  ["Dev"]="009800"
+  ["Test"]="FF7E3F"
+  ["Live"]="0A85CB"
+  ["Bug"]="fc2929"
+  ["Feature"]="E8FFA4"
+  ["Enhancement"]="84b6eb"
+  ["Ready+for+review"]="252759"
+  ["Released+for+customer+review"]="291B52"
+  ["Markup"]="fad8c7"
+  ["Invalid"]="e6e6e6"
+  ["Duplicated"]="cccccc"
+  ["Needs+clarification"]="cc317c"
+  ["Missing+Design+Materials"]="5319e7"
+  ["CSS"]="fbca04"
+  ["Priority"]="6B191C"
+  ["Can't+reproduce"]="20A74A"
+)
+
+
+# Delete default labels
+curl --user "$USER:$PASS" --include --request DELETE "https://api.github.com/repos/$REPO_USER/$REPO_NAME/labels/bug"
+curl --user "$USER:$PASS" --include --request DELETE "https://api.github.com/repos/$REPO_USER/$REPO_NAME/labels/duplicate"
+curl --user "$USER:$PASS" --include --request DELETE "https://api.github.com/repos/$REPO_USER/$REPO_NAME/labels/enhancement"
+curl --user "$USER:$PASS" --include --request DELETE "https://api.github.com/repos/$REPO_USER/$REPO_NAME/labels/help%20wanted"
+curl --user "$USER:$PASS" --include --request DELETE "https://api.github.com/repos/$REPO_USER/$REPO_NAME/labels/invalid"
+curl --user "$USER:$PASS" --include --request DELETE "https://api.github.com/repos/$REPO_USER/$REPO_NAME/labels/question"
+curl --user "$USER:$PASS" --include --request DELETE "https://api.github.com/repos/$REPO_USER/$REPO_NAME/labels/wontfix"
+
+# Create labels
+for key in ${!LABELS[@]}; do
+  NAME=$(echo ${key} | sed "s/+/ /g")
+  COLOR=${LABELS[${key}]}
+  DATA='{"name":"'$NAME'","color":"'$COLOR'"}'
+  curl --user "$USER:$PASS" --include --request POST --data "$DATA" "https://api.github.com/repos/$REPO_USER/$REPO_NAME/labels"
+done
--- src/issue-queue/index.md
+++ src/issue-queue/index.md
@@ -24,3 +24,16 @@ The following standard tags are used to mark issues:
 1. <span class="label-tag label-tag-css">CSS</span>
 1. <span class="label-tag label-tag-priority dark-background">Priority</span>
 1. <span class="label-tag label-tag-cant-reproduce dark-background">Can't reproduce</span>
+
+### You can use the ***default labels script*** to easily set the default labels on your repository:
+
+```
+curl -O https://raw.githubusercontent.com/Gizra/TheGizraWay/master/bash-scripts/default_labels.sh && bash default_labels.sh
+```
+Then just follow the instructions.
+
+You'll need to provide some **Github** information:
+
+1. Username
+1. Password
+1. Repository name exactly as on *Github* (e.g. *TheGizraWay*)
